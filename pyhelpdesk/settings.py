452c6004555ff9c6d184f08282b4d6e838d158f7
Aaron Langley <alangley@gmail.com>
2022-05-26 12:03:35 -0400

    Changing my folder structure

--- pyhelpdesk/settings.py
+++ pyhelpdesk/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-!nat4sn^1a%1(ujmm)ixms!aufp0c7p71!^s%wdye%w(cr(vt!'
+SECRET_KEY = 'django-insecure-p#%252j%!iqmhkh)z=#50k08g*k7f=x#ypw^i2jgw$-x1-%c(f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
