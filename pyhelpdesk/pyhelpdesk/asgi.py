51fd117593a77b140903a5a8bab67f0531dfcaae
Aaron Langley <alangley@gmail.com>
2022-05-26 12:00:11 -0400

    Starting over

--- pyhelpdesk/pyhelpdesk/asgi.py
+++ pyhelpdesk/pyhelpdesk/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for py_helpdesk project.
+ASGI config for pyhelpdesk project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'py_helpdesk.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pyhelpdesk.settings')
 
 application = get_asgi_application()
